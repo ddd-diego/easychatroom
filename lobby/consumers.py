5a7cde419ca10e682ab948b957086d2dceed3fc5
ddds-typo <dddiego@zohomail.eu>
2025-10-21 20:54:52 +0200

    Update consumers.py

--- lobby/consumers.py
+++ lobby/consumers.py
@@ -10,25 +10,17 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.lobby_name=self.scope["url_route"]["kwargs"]["lobby_id"]
         self.lobby_group_name=f"lobby_{self.lobby_name}"
 
-        
+
         await self.accept()
         await self.channel_layer.group_add(self.lobby_group_name,self.channel_name)
 
         cookies=get_cookies(self.scope)
         self.chat_guestname=cookies.get("chat_guestname", "Anonymous")
         self.client_ip=self.scope['client'][0]
-        await self.user_data()
         connected_users.setdefault(self.lobby_name, set()).add((self.chat_guestname, self.client_ip))
         print(connected_users)
-
-
-
-    async def user_data(self):
-        await self.send(text_data=json.dumps({
-            "chat_guestname":self.chat_guestname,
-            "client_ip":self.client_ip
-            }))
-
+        await self.send(text_data=json.dumps({lobby:list(users)for lobby, users in connected_users.items()}))        
+            
 
 
     async def receive(self, text_data):
